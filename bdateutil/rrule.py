682c57552706f44a392e055f0e3633fe4b65f7c9
ryanss <ryanssdev@icloud.com>
2014-11-11 14:06:44 -0500

    Add setup.py and MANIFEST.in preparing for first release

--- bdateutil/rrule.py
+++ bdateutil/rrule.py
@@ -1,3 +1,14 @@
+#  bdateutil
+#  -----------
+#  Adds business day logic and improved data type flexibility to
+#  python-dateutil. 100% backwards compatible with python-dateutil,
+#  simply replace dateutil imports with bdateutil.
+#
+#  Author:  ryanss <ryanssdev@icloud.com>
+#  Website: https://github.com/ryanss/bdateutil
+#  License: MIT (see LICENSE file)
+
+
 from dateutil.rrule import *
 from dateutil.rrule import rrule as rrulebase
 from dateutil.rrule import _rrulestr as rrulestrbase
---
+++ setup.py
@@ -0,0 +1,50 @@
+#  bdateutil
+#  -----------
+#  Adds business day logic and improved data type flexibility to
+#  python-dateutil. 100% backwards compatible with python-dateutil,
+#  simply replace dateutil imports with bdateutil.
+#
+#  Author:  ryanss <ryanssdev@icloud.com>
+#  Website: https://github.com/ryanss/bdateutil
+#  License: MIT (see LICENSE file)
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='bdateutil',
+    version='0.1-dev',
+    author='ryanss',
+    author_email='ryanssdev@icloud.com',
+    url='https://github.com/ryanss/bdateutil',
+    license='MIT',
+    packages=['bdateutil'],
+    description='Adds business day logic and improved data type flexibility \
+                 to python-dateutil.',
+    long_description=open('README.rst').read(),
+    install_requires=['python-dateutil', 'holidays'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Office/Business :: Scheduling',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization',
+    ],
+)
